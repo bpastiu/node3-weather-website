2a4ac9f8ac9ce9634599ce37e097d16fa4e27083
Bogdan Pastiu <bogdan.pastiu@toyota.com>
2021-04-11 15:14:00 +0300

    Adds humidity to weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=7914d03353ed5cc1a1c0e2ffb074cb5b&query=${latitude},${longitude}&units=m`
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location.", undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Air humidity level is ${body.current.humidity}.`);
         }
     })
 }
