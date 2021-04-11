90267e36119ca9207af45344fda977f4aa0009d8
Bogdan Pastiu <bogdan.pastiu@toyota.com>
2021-04-11 14:52:05 +0300

    Setup app for Heroky

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const forecast = require("./utils/forecast")
 
 const app = express();
 
+// heroku server port or fallback on 3000
+const port = process.env.PORT || 3000;
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public")
 const viewsPath = path.join(__dirname, "../templates/views")
@@ -95,6 +98,6 @@ app.get("*", (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000.")
+app.listen(port, () => {
+    console.log("Server is up on port " + port)
 });
